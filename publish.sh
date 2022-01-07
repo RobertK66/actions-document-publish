359a95c9465404477785bca5382e5616ab5f3d6d
RobertK66 <RobertK66@users.noreply.github.com>
2022-01-07 18:26:50 +0100

    add extended css to args

--- publish.sh
+++ publish.sh
@@ -1,27 +1,31 @@
 #!/bin/sh
+echo "Script called with args: '$0' '$1' '$2' '$3' '$4'"
 
 SOURCES=$1
 DOCS=".publish-docs/"
 DOCUMENT="${DOCS}/document.md"
-CONTENT="${DOCS}/content.md"
-STYLE="${DOCS}/style.md"
+#CONTENT="${DOCS}/content.md"
+#STYLE="${DOCS}/style.md"
 
-mkdir -p $DOCS && touch $DOCUMENT && touch $CONTENT && touch $STYLE
+mkdir -p $DOCS && touch $DOCUMENT 
+#&& touch $CONTENT && touch $STYLE
 
-echo "<style>@media print {.pagebreak { page-break-before: always; break-before: always; }}</style>" >> $STYLE
+#echo "<style>@media print {.pagebreak { page-break-before: always; break-before: always; }}</style>" >> $STYLE
 
-for file in $SOURCES; do (cat "${file}"; printf "\n<div class='pagebreak'>&nbsp;</div>\n\n") done > $CONTENT
+for file in $SOURCES; do (cat "${file}"; printf "\n<div class='pagebreak'>&nbsp;</div>\n\n") done > $DOCUMENT
 
-cat $STYLE >> $DOCUMENT
-cat $CONTENT >> $DOCUMENT
+#cat $STYLE >> $DOCUMENT
+#cat $CONTENT >> $DOCUMENT
 
-echo "Script called with args: '$0' '$1' '$2' '$3'"
+
 
 INPUT_INPUT_DIR=$DOCS \
 INPUT_OUTPUT_DIR=$DOCS \
 INPUT_BUILD_HTML=false \
 INPUT_IMAGES_DIR=$2 \
 INPUT_IMAGE_IMPORT=$3 \
+INPUT_THEME=$4 \
+INPUT_EXTEND_DEFAULT_THEME=true \
 /usr/local/bin/markdown-to-pdf
 
 echo "::set-output name=pdf::${DOCS}/document.pdf"
