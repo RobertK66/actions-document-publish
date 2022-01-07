1936f0675c451dcddbef0a128cd50202d1e841e4
RobertK66 <RobertK66@users.noreply.github.com>
2022-01-07 17:22:21 +0100

    use both image args

--- publish.sh
+++ publish.sh
@@ -8,13 +8,13 @@ mkdir -p $DOCS && touch $DOCUMENT
 
 for file in $SOURCES; do (cat "${file}"; echo) done > $DOCUMENT
 
-echo "Hello its me! Args: $0 $1 $2 $3"
+echo "Script called with args: '$0' '$1' '$2' '$3'"
 
 INPUT_INPUT_DIR=$DOCS \
 INPUT_OUTPUT_DIR=$DOCS \
 INPUT_BUILD_HTML=false \
-INPUT_IMAGES_DIR=doc/pic \
-INPUT_IMAGE_IMPORT=pic \
+INPUT_IMAGES_DIR=$1 \
+INPUT_IMAGE_IMPORT=$2 \
 /usr/local/bin/markdown-to-pdf
 
 echo "::set-output name=pdf::${DOCS}/document.pdf"
